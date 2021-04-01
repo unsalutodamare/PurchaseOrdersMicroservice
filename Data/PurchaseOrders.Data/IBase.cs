1bf6a12d2452dd41b4a10e71833880757a80d5e3
LUKA MINIC <luka.minic@gmail.com>
2021-04-02 00:50:41 +0200

    Setting up the project and adding the IBase class

---
+++ Data/PurchaseOrders.Data/IBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchaseOrders.Data
+{
+    public interface IBase
+    {
+        public int Id { get; set; }
+
+        DateTimeOffset Created { get; set; }
+        DateTimeOffset? LastUpdated { get; set; }
+    }
+}
