fb440b753afc3c941b3ef140aeacde9ecf258d98
LUKA MINIC <luka.minic@gmail.com>
2021-04-02 02:10:07 +0200

    Configuring connection strings and appsettings

--- PurchaseOrdersWeb/Startup.cs
+++ PurchaseOrdersWeb/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using PurchaseOrders.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,9 @@ namespace PurchaseOrdersWeb
         {
 
             services.AddControllers();
+
+            services.AddDbContext<PurchaseOrdersDbContext>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PurchaseOrdersWeb", Version = "v1" });
